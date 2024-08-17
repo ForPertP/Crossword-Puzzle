ef555f4dd6fd5485cdba99ecad260cfda8cc6f57
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-08-18 02:37:15 +0900

    Update Crossword-Puzzle.cpp

--- Crossword-Puzzle.cpp
+++ Crossword-Puzzle.cpp
@@ -89,3 +89,36 @@ vector<string> crosswordPuzzle(vector<string> crossword, string words)
     return crossword;
 }
 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<string> crossword(10);
+
+    for (int i = 0; i < 10; i++) {
+        string crossword_item;
+        getline(cin, crossword_item);
+
+        crossword[i] = crossword_item;
+    }
+
+    string words;
+    getline(cin, words);
+
+    vector<string> result = crosswordPuzzle(crossword, words);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
