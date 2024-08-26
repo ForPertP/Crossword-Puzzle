8e54250aff4ef3a8ad8ffb2a4f06265964d86a24
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-08-27 03:48:03 +0900

    Update Crossword-Puzzle.cs

--- Crossword-Puzzle.cs
+++ Crossword-Puzzle.cs
@@ -1,1 +1,32 @@
-Crossword-Puzzle C#
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'crosswordPuzzle' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY crossword
+     *  2. STRING words
+     */
+
+    public static List<string> crosswordPuzzle(List<string> crossword, string words)
+    {
+
+    }
+
+}
