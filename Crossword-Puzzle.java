71ef23e08dd98e7496a72fc39d994503e32eb698
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-09-15 01:04:23 +0900

    Update Crossword-Puzzle.java

--- Crossword-Puzzle.java
+++ Crossword-Puzzle.java
@@ -23,6 +23,69 @@ class Result {
 
     static boolean solveCrossword(List<String> crossword, List<String> wordList) {
         if (wordList.size() == 0) return true;
+
+        String word = wordList.get(wordList.size() - 1);
+        int len = word.length();
+
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                if (j + len <= 10) {
+                    List<String> backup = new ArrayList<>(crossword);
+
+                    boolean canPlace = true;
+                    for (int k = 0; k < len; ++k) {
+                        if (crossword.get(i).charAt(j + k) != '-' && crossword.get(i).charAt(j + k) != word.charAt(k)) {
+                            canPlace = false;
+                            break;
+                        }
+                    }
+
+                    if (canPlace) {
+                        StringBuilder sb = new StringBuilder(crossword.get(i));
+                        for (int k = 0; k < len; ++k) {
+                            sb.setCharAt(j + k, word.charAt(k));
+                        }
+                        crossword.set(i, sb.toString());
+
+                        wordList.remove(wordList.size() - 1);
+                        if (solveCrossword(crossword, wordList)) return true;
+                        wordList.add(word);
+
+                        crossword.clear();
+                        crossword.addAll(backup);
+                    }
+                }
+
+
+                if (i + len <= 10) {
+                    List<String> backup = new ArrayList<>(crossword);
+
+                    boolean canPlace = true;
+                    for (int k = 0; k < len; ++k) {
+                        if (crossword.get(i + k).charAt(j) != '-' && crossword.get(i + k).charAt(j) != word.charAt(k)) {
+                            canPlace = false;
+                            break;
+                        }
+                    }
+
+                    if (canPlace) {
+                        for (int k = 0; k < len; ++k) {
+                            StringBuilder sb = new StringBuilder(crossword.get(i + k));
+                            sb.setCharAt(j, word.charAt(k));
+                            crossword.set(i + k, sb.toString());
+                        }
+
+                        wordList.remove(wordList.size() - 1);
+                        if (solveCrossword(crossword, wordList)) return true;
+                        wordList.add(word);
+
+                        crossword.clear();
+                        crossword.addAll(backup);
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 
